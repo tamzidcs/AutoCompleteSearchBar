40428155a9fdee78a2b4b5d301b58ad1449661a1
osboxes.org <tamzid_cse@yahoo.com>
2021-07-14 17:02:44 -0400

    initialial commit with auto complete search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
-@Component({
+/**
+ * @title Plain input autocomplete
+ */
+ import {Component, OnInit} from '@angular/core';
+ import {FormControl} from '@angular/forms';
+ import {Observable} from 'rxjs';
+ import {startWith, map} from 'rxjs/operators';
+ @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AutoCompleteSearchBar';
+export class AppComponent implements OnInit {
+  control = new FormControl();
+  cities: string[] = ['New York', 'Chicago', 'San Fransisco', 'Seatle'];
+  filteredCities: Observable<string[]>=new Observable<string[]>();
+
+  ngOnInit() {
+    this.filteredCities = this.control.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = this._normalizeValue(value);
+    return this.cities.filter(cities => this._normalizeValue(cities).includes(filterValue));
+  }
+
+  private _normalizeValue(value: string): string {
+    return value.toLowerCase().replace(/\s/g, '');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
@@ -10,9 +23,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule, 
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
-})
+}) 
 export class AppModule { }
